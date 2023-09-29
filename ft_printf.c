6a503579d4eef94d4f0f48aa53f9192ef42cdd03
pmelo-ca <pedromelocf@yahoo.com>
2023-09-29 18:34:15 -0300

    First version NOT WORKING

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:50:49 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/26 16:32:05 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/29 18:33:46 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,29 @@
 
 int	ft_printf(const char *str, ...)
 {
-	int i;
+	short int i;
+	int length;
 
 	i = 0;
-	while(str[i] != '\0')
+	length = 0;
+	while (str[i])
 	{
-		write(1, &str[i], 1);
-		i++;
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'c')
+				putchar('c');
+			else if (str[i] == 's')
+			else if (str[i] == 'p')
+			else if (str[i] == 'd')
+			else if (str[i] == 'i')
+			else if (str[i] == 'u')
+			else if (str[i] == 'x')
+			else if (str[i] == 'X')
+			else if (str[i] == '%')
+		}
+		else
+			write(1, &str[i], 1);
 	}
-	return (0);
+	return (length);
 }
