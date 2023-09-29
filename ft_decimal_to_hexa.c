114d7765df81fc6b42e261fc71eef3882d1f15a2
pmelo-ca <pedromelocf@yahoo.com>
2023-09-29 16:46:49 -0300

    Converting decimal to hexa

---
+++ ft_decimal_to_hexa.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 15:44:22 by pmelo-ca          #+#    #+#             */
+/*   Updated: 2023/09/29 16:44:25 by pmelo-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_decimal_to_hexa(int nbr)
+{
+	char	*base;
+	char	hexnumber[50];
+	int		index;
+	int		reminder;
+
+	base = "0123456789abcdef";
+	index = 0;
+	while (nbr > 0)
+	{
+		reminder = nbr % 16;
+		hexnumber[index++] = base[reminder];
+		nbr /= 16;
+	}
+	return;
+}
+int	main(void)
+{
+	ft_decimal_to_hexa(18500);
+	return (0);
+}
