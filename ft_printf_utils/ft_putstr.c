e1028b909854fbbabf26a260a84d0fbadf306322
pmelo-ca <pedromelocf@yahoo.com>
2023-09-30 17:06:12 -0300

    Inserting ft_putstr

--- ft_printf_utils/ft_putstr.c
+++ ft_printf_utils/ft_putstr.c
@@ -6,7 +6,20 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 17:00:03 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/30 17:00:04 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:03:19 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ft_printf.h"
+
+void	ft_putstr(char *str)
+{
+	int	lenght;
+
+	lenght = 0;
+	while (str[lenght])
+	{
+		ft_putchar(str[lenght]);
+		lenght++;
+	}
+}
