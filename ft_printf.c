80940b32f95025d9fbafa8c99f7ad01eaa944958
pmelo-ca <pedromelocf@yahoo.com>
2023-09-30 16:43:05 -0300

    Logic

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:50:49 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/30 15:48:40 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/30 16:42:45 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_printf(const char *str, ...)
 {
 	short int	i;
 	int			length;
+	va_list		args;
 
 	i = 0;
 	length = 0;
@@ -27,9 +28,21 @@ int	ft_printf(const char *str, ...)
 			if (str[i] == '#' || str[i] == '+' || str[i] == ' ')
 				i++;
 			if (str[i] == 'c')
-				putchar('c');
+				write(1, str[i]);
 			else if (str[i] == 's')
-				else if (str[i] == 'p') else if (str[i] == 'd') else if (str[i] == 'i') else if (str[i] == 'u') else if (str[i] == 'x') else if (str[i] == 'X') else if (str[i] == '%')
+				putstring;
+			else if (str[i] == 'p')
+			
+			else if (str[i] == 'd' || str[i] == 'i')
+				putnumber;
+			else if (str[i] == 'u')
+				putnumber;
+			else if (str[i] == 'x')
+				ft_putnbr_base();
+			else if (str[i] == 'X')
+				ft_putnbr_base();
+			else if (str[i] == '%')
+				write(1, '%', 1);
 		}
 		else
 			write(1, &str[i], 1);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:51:05 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/29 18:26:25 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/30 16:38:33 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 # define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdio.h>
 # include "./libft/libft.h"
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
 
-int	ft_printf(const char *str, ...);
+int		ft_printf(const char *str, ...);
+void	ft_putnbr_base(int nbr, char *base);
 
 #endif
