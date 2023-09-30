8ea6f153c5c3148b585f392527f3fd47aca915b7
pmelo-ca <pedromelocf@yahoo.com>
2023-09-30 17:07:24 -0300

    Putchar

--- ft_printf_utils/ft_putchar.c
+++ ft_printf_utils/ft_putchar.c
@@ -6,9 +6,13 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 16:58:27 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/30 17:05:09 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:07:01 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
