82bd3ad797c3bc60fbccb3bad8a96697cca9fe0c
Pedro Melo <pedromelocf@yahoo.com>
2023-10-02 14:57:27 -0300

    Fixing logic

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:50:49 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/10/02 14:53:09 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/10/02 14:57:00 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,21 +28,21 @@ int	ft_printf(const char *str, ...)
 			i++;
 			if (str[i] == '#' || str[i] == '+' || str[i] == ' ')
 				i++;
-			else if (str[i++] == 'c')
+			else if (str[i] == 'c')
 				length += ft_putchar(va_arg(args, int));
-			else if (str[i++] == 's')
+			else if (str[i] == 's')
 				length += ft_putstr(va_arg(args, char *));
-			else if (str[i++] == 'p')
+			else if (str[i] == 'p')
 				length += ft_putstr(va_arg(args, char *));
-			else if (str[i++] == 'd' || str[i++] == 'i')
+			else if (str[i] == 'd' || str[i] == 'i')
 				length += ft_putnbr(va_arg(args, int));
-			else if (str[i++] == 'u')
+			else if (str[i] == 'u')
 				length += ft_putnbr(va_arg(args, unsigned int));
-			else if (str[i++] == 'x')
+			else if (str[i] == 'x')
 				length += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
-			else if (str[i++] == 'X')
+			else if (str[i] == 'X')
 				length += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
-			else if (str[i++] == '%')
+			else if (str[i] == '%')
 				length += ft_putchar('%');
 		}
 		else
@@ -51,8 +51,8 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (length);
 }
-// int	main(void)
-// {
-// 	ft_printf("%c", 'e');
-// 	return (1);
-// }
+int	main(void)
+{
+	ft_printf("%c%d%s%x%X%%", 'e', 42, "Hello World!", 42, 42);
+	return (1);
+}
