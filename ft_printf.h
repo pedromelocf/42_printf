2c23aa69060bf523247f416691882580911f51e3
Pedro Melo <pedromelocf@yahoo.com>
2023-10-02 19:44:26 -0300

    Inserting prototype to header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:51:05 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/09/30 17:32:03 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/10/02 19:40:36 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int	ft_putnbr_base(int nbr, char *base);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int nb);
+int	ft_put_unsigned(unsigned int nb);
 
 #endif
