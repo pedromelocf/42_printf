2def0fc35aed985231bfee6da4c7d5fabb1bdd33
Pedro Melo <pedromelocf@yahoo.com>
2023-10-03 16:06:53 -0300

    Fixing errors

--- ft_printf_utils/ft_put_unsigned.c
+++ ft_printf_utils/ft_put_unsigned.c
@@ -2,17 +2,21 @@
 
 int	ft_put_unsigned(unsigned int nb)
 {
+	int length;
+
+	length = 0;
 	if (nb < 0)
 	{
 		nb = 4294967295 + nb + 1;
 		ft_putnbr(nb);
 	}
-	if (nb < 10)
+	if ((nb < 10) && (nb >= 0))
 		ft_putchar(nb + '0');
 	else
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
 	}
-	return (0);
+	length += ft_intlen(nb);
+	return (length);
 }
--- ft_printf_utils/ft_put_void_pointer.c
+++ ft_printf_utils/ft_put_void_pointer.c
@@ -4,9 +4,9 @@ int ft_put_voidpointer(char *str)
 {
 	int length;
 
+	length = 0;
 	if (!str)
 		return (ft_putstr("(nil)"));
-	length = 0;
 	length += ft_putstr("0x");
 	length += ft_putnbr_base((unsigned long long int)str, "0123456789ABCDEF");
 	return (length);
