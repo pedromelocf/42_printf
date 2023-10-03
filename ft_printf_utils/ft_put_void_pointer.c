584f33e16771d60d26c953fbf1bc161e2f5f46cf
Pedro Melo <pedromelocf@yahoo.com>
2023-10-02 21:00:14 -0300

    Dealing with null pointer

--- ft_printf_utils/ft_put_void_pointer.c
+++ ft_printf_utils/ft_put_void_pointer.c
@@ -4,6 +4,8 @@ int ft_put_voidpointer(char *str)
 {
 	int length;
 
+	if (!str)
+		return (ft_putstr("(nil)"));
 	length = 0;
 	length += ft_putstr("0x");
 	length += ft_putnbr_base((unsigned long long int)*str, "0123456789ABCDEF");
