dd7a4e75b911df937ffad1fbde565ee9e4fc341b
pmelo-ca <pedromelocf@yahoo.com>
2023-10-04 15:34:26 -0300

    Fixing %u errors

--- ft_printf_utils/ft_put_unsigned.c
+++ ft_printf_utils/ft_put_unsigned.c
@@ -2,13 +2,13 @@
 
 int	ft_put_unsigned(long int nb)
 {
-	int length;
+	int	length;
 
 	length = 0;
 	if (nb < 0)
 		nb = 4294967295 + nb + 1;
 	if (nb < 10)
-		ft_putchar(nb + '0');
+		length += ft_putchar(nb + '0');
 	else
 	{
 		length += ft_putnbr(nb / 10);
