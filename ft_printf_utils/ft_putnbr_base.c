77202a280d20cb6c6818129e8d1d7b1f07b61835
pmelo-ca <pedromelocf@yahoo.com>
2023-10-04 13:24:28 -0300

    Adding null terminator to hexnumber string and changing allocation size for dealing all numbers possible

--- ft_printf_utils/ft_putnbr_base.c
+++ ft_printf_utils/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:44:22 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/10/03 21:54:30 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/10/04 13:23:49 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_putnbr_base(int nbr, char *base)
 
 	base_len = ft_strlen(base);
 	index = 0;
-	hexnumber = malloc(sizeof(char) * 9);
+	hexnumber = malloc(sizeof(char) * 32);
 	if (nbr < 0)
 	{
 		write(1, "-", 1);
@@ -33,10 +33,10 @@ int	ft_putnbr_base(int nbr, char *base)
 		hexnumber[index++] = base[reminder];
 		nbr /= base_len;
 	}
+	hexnumber[index] = '\0';
 	while (index > 0)
 		write(1, &hexnumber[--index], 1);
 	base_len = ft_strlen(hexnumber);
-	// hexnumber = NULL;
 	free(hexnumber);
 	return (base_len);
 }
