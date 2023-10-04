ed702dd9682275f57ee186057c73e54f01ad67c9
pmelo-ca <pedromelocf@yahoo.com>
2023-10-04 18:00:43 -0300

    Fixing %x errors

--- ft_printf_utils/ft_putnbr_base.c
+++ ft_printf_utils/ft_putnbr_base.c
@@ -6,27 +6,29 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:44:22 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/10/04 16:37:08 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:58:49 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int	ft_putnbr_base(unsigned long int nbr, char *base)
+int	ft_putnbr_base(unsigned int nbr, char *base)
 {
-	char	*hexnumber;
-	int		index;
-	int		reminder;
-	int		base_len;
+	char			*hexnumber;
+	int				index;
+	unsigned int	reminder;
+	int				base_len;
 
 	base_len = ft_strlen(base);
 	index = 0;
-	hexnumber = malloc(sizeof(char) * 32);
 	if (nbr < 0)
 	{
 		ft_putchar('-');
 		nbr *= -1;
 	}
+	if (nbr == 0)
+		return (ft_putchar('0'));
+	hexnumber = malloc(sizeof(char) * 32);
 	while (nbr > 0)
 	{
 		reminder = nbr % base_len;
