6dcd569e586d197e4f328d025443c49e353a7089
pmelo-ca <pedromelocf@yahoo.com>
2023-10-04 14:23:48 -0300

    Dealing with null params

--- ft_printf_utils/ft_putstr.c
+++ ft_printf_utils/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 17:00:03 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/10/03 16:23:10 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/10/04 14:22:24 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_putstr(char *str)
 	int	length;
 
 	length = 0;
+	if (!str)
+		return (ft_putstr("(null)"));
 	while (str[length])
 	{
 		ft_putchar(str[length]);
